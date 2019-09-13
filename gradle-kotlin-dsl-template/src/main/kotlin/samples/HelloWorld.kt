bc867294f0a02690b4ceff0bca3bf15d2fad7d25
Tamas Lang <tamas@talangsoft.org>
2019-09-13 14:49:20 +0100

    add a few more sample tests

--- gradle-kotlin-dsl-template/src/main/kotlin/samples/HelloWorld.kt
+++ gradle-kotlin-dsl-template/src/main/kotlin/samples/HelloWorld.kt
@@ -4,4 +4,4 @@ fun main(args: Array<String>) {
     println(sayHello("World"))
 }
 
-fun sayHello(name: String) = "Hello $name"
+fun sayHello(name: String = "") = "Hello $name".trimEnd()
--- gradle-kotlin-dsl-template/src/test/kotlin/samples/HelloWorldKtTest.kt
+++ gradle-kotlin-dsl-template/src/test/kotlin/samples/HelloWorldKtTest.kt
@@ -6,8 +6,21 @@ import org.junit.jupiter.api.Test
 class HelloWorldKtTest{
 
     @Test
-    fun `hello should say hello to input name`() {
+    fun `sayHello should say hello to input name 'John'`() {
         val name = "John"
         assertThat(sayHello(name)).isEqualTo("Hello John")
     }
+
+    @Test
+    fun `sayHello should say hello to an empty input name`() {
+        val name = ""
+        assertThat(sayHello(name)).isEqualTo("Hello")
+    }
+
+    @Test
+    fun `sayHello should say hello to an empty input name by default parameter`() {
+        assertThat(sayHello()).isEqualTo("Hello")
+    }
+
+
 }
