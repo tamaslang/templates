c111440b88db6fc1710e4c8894b66c5d51e1dafa
Tamas Lang <tamas@talangsoft.org>
2020-06-09 14:50:11 +0100

    Update dependency versions

--- gradle-kotlin-dsl-template/build.gradle.kts
+++ gradle-kotlin-dsl-template/build.gradle.kts
@@ -3,17 +3,17 @@ import io.gitlab.arturbosch.detekt.detekt
 
 plugins {
     application
-    kotlin("jvm") version "1.3.50"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    id ("com.adarshr.test-logger") version "1.7.0"
-    id ("io.gitlab.arturbosch.detekt") version "1.0.1"
+    kotlin("jvm") version "1.3.72"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id ("com.adarshr.test-logger") version "2.0.0"
+    id ("io.gitlab.arturbosch.detekt") version "1.5.0"
 }
 
 application {
     mainClassName = "samples.HelloWorldKt"
 }
 
-val assertjVersion = "3.11.1"
+val assertjVersion = "3.15.0"
 val junitVersion = "5.3.2"
 val detektVersion = "1.0.1"
 val junitVintageVersion = "4.12"
